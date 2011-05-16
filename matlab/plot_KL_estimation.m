e17aa744ff9213e9954a4a51a804225682e6a9db
Pietro Berkes <pietro.berkes@googlemail.com>
2011-05-16 11:58:54 -0400

    NEW: Matlab function to visualize KL extrapolation

---
+++ matlab/plot_KL_estimation.m
@@ -0,0 +1,27 @@
+function plot_KL_estimation(KL_means, N)
+% PLOT_KL_ESTIMATION visualize KL extrapolation results
+%   
+%
+%   PLOT_KL_ESTIMATION(KL_means, N) plots the intermediate results of the
+%    KL estimation process for visual inspection and diagnosis.
+%
+%   Parameters:
+%   KL_means: mean KL estimates for N/4, N/2, and N;
+%             returned by KL_ESTIMATION
+%   N: vector with number of points used for extrapolation;
+%             returned by KL_ESTIMATION
+
+% Copyright (c) 2011 Pietro Berkes and Dmitriy Lisitsyn
+% License: GPL v3
+
+    [p,S,mu] = polyfit(N,(N.*N).*KL_means',2);
+    kl = p(1)/(mu(2).^2);
+
+    x = linspace(N(1)/2, N(end)*5, 100);
+    y = polyval(p, x, S, mu);
+    clf;
+    hold on;
+    plot(x, y./(x.^2), 'b:');
+    plot(N, KL_means,'bo');
+    plot([x(1), x(end)], [kl kl], 'r--');
+    
